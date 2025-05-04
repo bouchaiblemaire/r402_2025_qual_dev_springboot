74b87c513702c4f81a61417805b8658f899fef69
bl <bouchkhaf@gmail.com>
2025-05-04 17:09:09 +0200

    Ajout de la classe métier et de sa classe de tests

---
+++ src/main/java/com/example/springbootest/metier/Voiture.java
@@ -0,0 +1,61 @@
+package com.example.springbootest.metier;
+
+
+import java.util.Objects;
+
+public class Voiture {
+
+    private String marque;
+    private double prix;
+
+
+    public Voiture(){
+    }
+
+    /**
+     * Constructeur
+     *
+     * L'id est généré automatiquement
+     * @param marque
+     * @param prix
+     */
+    public Voiture(String marque, double prix) {
+        this.marque = marque;
+        this.prix = prix;
+    }
+
+
+
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+
+    @Override
+    public String toString() {
+        return "\"Car\":{"
+                + "\"marque\":\"" + marque + "\","
+                + "\"prix\":" + prix +"}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Voiture voiture = (Voiture) o;
+        return Double.compare(getPrix(), voiture.getPrix()) == 0 && Objects.equals(getMarque(), voiture.getMarque());
+    }
+}
+
---
+++ src/test/java/com/example/springbootest/metier/VoitureTest.java
@@ -0,0 +1,93 @@
+package com.example.springbootest.metier;
+
+import com.example.springbootest.metier.Voiture;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class VoitureTest {
+
+    /**
+     * @Test
+     * Annotation utilisé pour indiqué que la méthode effectue un test
+     */
+
+    @Test
+    void when_getprix_should_return_same_price_of_instanciate_car(){
+        // Arrange
+        Voiture voiture = new Voiture("Ferrari", 5000);
+
+        // Act
+        double expectedPrice = 5000;
+        double price = voiture.getPrix();
+
+
+        // Verify
+        Assertions.assertEquals(price, expectedPrice, "Doit être " + expectedPrice);
+    }
+
+    @Test
+    void when_getmarque_should_return_same_marque_of_instanciate_car(){
+        // Arrange
+        Voiture voiture = new Voiture("Ferrari", 5000);
+
+        // Act
+        String expectedMarque = "Ferrari";
+        String marque= voiture.getMarque();
+
+
+        // Verify
+        Assertions.assertEquals(marque, expectedMarque, "Doit être " + expectedMarque);
+    }
+
+    @Test
+    void when_get_prix_after_set_prix_should_return_same_price(){
+        // Arrange
+        Voiture voiture = new Voiture();
+        voiture.setPrix(5000);
+
+        // Act
+        double expectedPrice = 5000;
+        double price = voiture.getPrix();
+
+
+        // Verify
+        Assertions.assertEquals(price, expectedPrice, "Doit être " + expectedPrice);
+    }
+
+
+    @Test
+    void when_get_marque_after_set_marque_should_return_same_marque(){
+        // Arrange
+        Voiture voiture =  new Voiture();
+        voiture.setMarque("Ferrari");
+
+        // Act
+        String expectedMarque = "Ferrari";
+        String marque = voiture.getMarque();
+
+
+        // Verify
+        Assertions.assertEquals(marque, expectedMarque, "Doit être " + expectedMarque);
+    }
+
+
+
+
+    @Test
+    void when_tostring_should_return_correct_string(){
+        // Arrange
+        Voiture voiture = new Voiture("Ferrari", 5000);
+
+        // Act
+        String expecteString =  "\"Car\":{\"marque\":\"Ferrari\",\"prix\":5000.0}";
+
+        String stringFromToString= voiture.toString();
+
+
+        // Verify
+        // La clef primaire est générée par la courche de persistance JPA
+        Assertions.assertEquals(stringFromToString,expecteString, "Doit être " + expecteString);
+    }
+
+}
